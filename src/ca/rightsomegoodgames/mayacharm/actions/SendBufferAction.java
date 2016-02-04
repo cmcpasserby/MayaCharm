02eae16b57ea9e20c3b96fa941c9ea044c5692ed
Chris Cunningham <chris@rightsomegoodgames.ca>
2016-02-04 10:25:31 -0400

    Simplified the send buffer action, not it passes the current doc directly instead of just creating a temp file to pass

--- src/ca/rightsomegoodgames/mayacharm/actions/SendBufferAction.java
+++ src/ca/rightsomegoodgames/mayacharm/actions/SendBufferAction.java
@@ -4,22 +4,19 @@ import ca.rightsomegoodgames.mayacharm.mayacomms.MayaCommInterface;
 import ca.rightsomegoodgames.mayacharm.settings.MCSettingsProvider;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+
 
 public class SendBufferAction extends BaseSendAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         final MCSettingsProvider settings = MCSettingsProvider.getInstance(e.getProject());
         final VirtualFile data = e.getData(LangDataKeys.VIRTUAL_FILE);
-        Document document = null;
-        if (data != null) document = FileDocumentManager.getInstance().getDocument(data);
-        if (document != null) {
+        if (data != null) {
             MayaCommInterface maya = new MayaCommInterface(settings.getHost(), settings.getPort());
             maya.connectMayaLog();
-            maya.sendCodeToMaya(document.getText());
+            maya.sendFileToMaya(data.getPath());
         }
     }
 }
