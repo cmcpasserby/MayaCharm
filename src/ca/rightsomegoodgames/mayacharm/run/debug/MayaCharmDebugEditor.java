8e55541e3cfe2413f12254aa1a588a77e97dfa54
Chris Cunningham <chris@rightsomegoodgames.ca>
2016-02-04 21:10:50 -0400

    debug editor

--- src/ca/rightsomegoodgames/mayacharm/run/debug/MayaCharmDebugEditor.java
+++ src/ca/rightsomegoodgames/mayacharm/run/debug/MayaCharmDebugEditor.java
@@ -1,11 +1,22 @@
 package ca.rightsomegoodgames.mayacharm.run.debug;
 
 import com.intellij.openapi.project.Project;
-import com.jetbrains.python.debugger.remote.PyRemoteDebugConfiguration;
 import com.jetbrains.python.debugger.remote.PyRemoteDebugConfigurationEditor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 public class MayaCharmDebugEditor extends PyRemoteDebugConfigurationEditor {
-    public MayaCharmDebugEditor(Project project, PyRemoteDebugConfiguration pyRemoteDebugConfiguration) {
-        super(project, pyRemoteDebugConfiguration);
+    private MayaCharmDebugForm myForm;
+
+    public MayaCharmDebugEditor(Project project, MayaCharmDebugConfig config) {
+        super(project, config);
+        myForm = new MayaCharmDebugForm(project, config);
+    }
+
+    @NotNull
+    @Override
+    protected JComponent createEditor() {
+        return myForm.GetPanel();
     }
 }
