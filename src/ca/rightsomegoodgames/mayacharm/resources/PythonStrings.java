520b344f8b7abc1b507c90061c7f37fcf17bd834
Chris Cunningham <chris@rightsomegoodgames.ca>
2016-02-08 17:30:07 -0400

    hopefully fixed issues with maya freezing on disconnect

--- src/ca/rightsomegoodgames/mayacharm/resources/PythonStrings.java
+++ src/ca/rightsomegoodgames/mayacharm/resources/PythonStrings.java
@@ -4,7 +4,8 @@ public final class PythonStrings {
     public static final String CONNECT_LOG = "import maya.cmds as cmds; cmds.cmdFileOutput(o=\"{0}\")";
     public static final String EXECFILE = "python (\"execfile (\\\"{0}\\\")\");";
 
-    public static final String SETTRACE = "import pydevd; pydevd.settrace('localhost', port=\"{0}\", suspend=False)";
+    public static final String SETTRACE = "import pydevd; pydevd.settrace(\"localhost\", port=%s, suspend=False)";
+//    public static final String SETTRACE = "import pydevd; pydevd.stoptrace(); pydevd.settrace(\"localhost\", port=%s, suspend=False)";
     public static final String STOPTRACE = "import pydevd; pydevd.stoptrace()";
 
     public static final String PYSTDERR = "# Error: ";
--- src/ca/rightsomegoodgames/mayacharm/run/debug/MayaCharmDebugProcess.java
+++ src/ca/rightsomegoodgames/mayacharm/run/debug/MayaCharmDebugProcess.java
@@ -1,5 +1,8 @@
 package ca.rightsomegoodgames.mayacharm.run.debug;
 
+import ca.rightsomegoodgames.mayacharm.mayacomms.MayaCommInterface;
+import ca.rightsomegoodgames.mayacharm.resources.PythonStrings;
+import ca.rightsomegoodgames.mayacharm.settings.MCSettingsProvider;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.ui.ExecutionConsole;
 import com.intellij.xdebugger.XDebugSession;
@@ -10,25 +13,48 @@ import org.jetbrains.annotations.Nullable;
 import java.net.ServerSocket;
 
 public class MayaCharmDebugProcess extends PyRemoteDebugProcess {
-    @Override
-    protected void beforeConnect() {
-        System.out.println("MayaCharm: Before_Connect");
-        super.beforeConnect();
+    private ServerSocket socket;
+    private MayaCommInterface mayaCommInterface;
+    private boolean canConnect;
+
+    public MayaCharmDebugProcess(@NotNull XDebugSession xDebugSession, @NotNull ServerSocket serverSocket, @NotNull ExecutionConsole executionConsole, @Nullable ProcessHandler processHandler, @Nullable String s) {
+        super(xDebugSession, serverSocket, executionConsole, processHandler, s);
+        socket = serverSocket;
     }
 
     @Override
-    protected void afterConnect() {
-        System.out.println("MayaCharm: After_Connect");
-        super.afterConnect();
+    public void sessionInitialized() {
+        super.sessionInitialized();
+        canConnect = true;
+    }
+
+    @Override
+    protected void beforeConnect() {
+        super.beforeConnect();
+        if (canConnect) {
+            MCSettingsProvider mcSettingsProvider = MCSettingsProvider.getInstance(getProject());
+            if (mcSettingsProvider != null) {
+                mayaCommInterface = new MayaCommInterface(mcSettingsProvider.getHost(), mcSettingsProvider.getPort());
+            }
+            if (mayaCommInterface != null) {
+                mayaCommInterface.connectMayaLog();
+                mayaCommInterface.sendCodeToMaya(String.format(PythonStrings.SETTRACE, socket.getLocalPort()));
+            }
+        }
     }
 
     @Override
     public void stop() {
-        System.out.println("MayaCharm: Stop");
+        canConnect = false;
+        if (mayaCommInterface != null) {
+            mayaCommInterface.sendCodeToMaya(PythonStrings.STOPTRACE);
+        }
         super.stop();
     }
 
-    public MayaCharmDebugProcess(@NotNull XDebugSession xDebugSession, @NotNull ServerSocket serverSocket, @NotNull ExecutionConsole executionConsole, @Nullable ProcessHandler processHandler, @Nullable String s) {
-        super(xDebugSession, serverSocket, executionConsole, processHandler, s);
+    @Override
+    protected void disconnect() {
+        canConnect = false;
+        super.disconnect();
     }
 }
