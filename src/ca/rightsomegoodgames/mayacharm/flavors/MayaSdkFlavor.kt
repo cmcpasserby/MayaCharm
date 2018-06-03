142cfce8d8474523b4a8bbd211aee8b92a14737c
Chris Cunningham <chris@rightsomegoodgames.ca>
2018-06-03 17:52:08 -0300

    added a PythonSdkFlavor for MayaPy

---
+++ src/ca/rightsomegoodgames/mayacharm/flavors/MayaSdkFlavor.kt
@@ -0,0 +1,52 @@
+package ca.rightsomegoodgames.mayacharm.flavors
+
+import com.jetbrains.python.sdk.flavors.PythonSdkFlavor
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.python.sdk.flavors.PythonFlavorProvider
+import icons.PythonIcons
+import java.io.File
+import javax.swing.Icon
+
+class MayaSdkFlavor private constructor() : PythonSdkFlavor() {
+    override fun isValidSdkHome(path: String): Boolean {
+        val file = File(path)
+        return file.isFile && isValidSdkPath(file) || isMayaFolder(file)
+    }
+
+    override fun isValidSdkPath(file: File): Boolean {
+        val name = FileUtil.getNameWithoutExtension(file).toLowerCase()
+        return name.startsWith("mayapy")
+    }
+
+    override fun getVersionOption(): String {
+        return  "--version"
+    }
+
+    override fun getName(): String {
+        return "MayaPy"
+    }
+
+    override fun getIcon(): Icon {
+        return PythonIcons.Python.Python
+    }
+
+    override fun getSdkPath(path: VirtualFile): VirtualFile? {
+        if (isMayaFolder(File(path.path))) {
+            return path.findFileByRelativePath("Contents/bin/mayapy")
+        }
+        return path
+    }
+
+    companion object {
+        var INSTANCE: MayaSdkFlavor = MayaSdkFlavor()
+
+        private fun isMayaFolder(file: File): Boolean {
+            return file.isDirectory && file.name == "Maya.app"
+        }
+    }
+}
+
+class MayaFlavorProvider : PythonFlavorProvider {
+    override fun getFlavor(platformIndependent: Boolean): PythonSdkFlavor? = MayaSdkFlavor.INSTANCE
+}
