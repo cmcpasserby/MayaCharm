4c1c85d87aca09cab650c0d29fbe0190f03b58cf
Chris Cunningham <chris.cunningham@gogiigames.com>
2018-11-05 09:52:29 -0400

    xdebugSession work, still need to figure out where i can pass in myPID and SDK home path

--- src/ca/rightsomegoodgames/mayacharm/run/MayaCharmDebugProcess.kt
+++ src/ca/rightsomegoodgames/mayacharm/run/MayaCharmDebugProcess.kt
@@ -1,8 +1,10 @@
 package ca.rightsomegoodgames.mayacharm.run
 
 import com.intellij.execution.process.ProcessHandler
+import com.intellij.execution.process.ProcessInfo
 import com.intellij.execution.ui.ConsoleViewContentType
 import com.intellij.execution.ui.ExecutionConsole
+import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.xdebugger.XDebugSession
 import com.jetbrains.python.debugger.PyDebugProcess
 import java.net.ServerSocket
@@ -11,7 +13,10 @@ class MayaCharmDebugProcess(session: XDebugSession,
                             serverSocket: ServerSocket,
                             executionConsole: ExecutionConsole,
                             processHandler: ProcessHandler?,
-                            multiProcess: Boolean)
+                            multiProcess: Boolean,
+                            private val sdk: Sdk,
+                            private val process: ProcessInfo
+                            )
                             : PyDebugProcess(session,
                                 serverSocket,
                                 executionConsole,
@@ -26,7 +31,7 @@ class MayaCharmDebugProcess(session: XDebugSession,
     }
 
     override fun getConnectionMessage(): String {
-        return "Attaching to a process with a PID=$myPid" // TODO: pass in from runner in constructor
+        return "Attaching to a process with a PID=${process.pid}" // TODO: pass in from runner in constructor
     }
 
     override fun getConnectionTitle(): String {
--- src/ca/rightsomegoodgames/mayacharm/run/MayaCharmDebugRunner.kt
+++ src/ca/rightsomegoodgames/mayacharm/run/MayaCharmDebugRunner.kt
@@ -2,15 +2,19 @@ package ca.rightsomegoodgames.mayacharm.run
 
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.execution.configurations.RunProfileState
-import com.intellij.execution.configurations.RunnerSettings
 import com.intellij.execution.process.impl.ProcessListUtil
 import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.execution.runners.GenericProgramRunner
 import com.intellij.execution.ui.RunContentDescriptor
-import com.jetbrains.python.debugger.attach.PyAttachToProcessDebugRunner
+import com.intellij.xdebugger.XDebugProcess
+import com.intellij.xdebugger.XDebugProcessStarter
+import com.intellij.xdebugger.XDebugSession
+import com.intellij.xdebugger.XDebuggerManager
+import com.jetbrains.python.debugger.PyDebugRunner
+import com.jetbrains.python.debugger.PyLocalPositionConverter
 import com.jetbrains.python.sdk.PythonSdkType
+import java.net.ServerSocket
 
-class MayaCharmDebugRunner : GenericProgramRunner<RunnerSettings>() {
+class MayaCharmDebugRunner : PyDebugRunner() {
     override fun getRunnerId(): String {
         return "MayaCharmDebugRunner"
     }
@@ -20,14 +24,39 @@ class MayaCharmDebugRunner : GenericProgramRunner<RunnerSettings>() {
     }
 
     override fun doExecute(state: RunProfileState, environment: ExecutionEnvironment): RunContentDescriptor? {
-        val result = PythonSdkType.getAllLocalCPythons().first { it.homePath!!.contains("mayapy.exe") }
+        val sdk = PythonSdkType.getAllLocalCPythons().first { it.homePath!!.contains("mayapy.exe") }
         val process = ProcessListUtil.getProcessList().first { it.executableName == "maya.exe" }
 
-        // TODO: Setup a proper XDebugSession with process starter
+        val serverSocket = ServerSocket(0)
+        val executionResult = state.execute(environment.executor, this)
 
-        val attachRunner = PyAttachToProcessDebugRunner(environment.project, process.pid, result.homePath)
-        attachRunner.launch()
+        val session = XDebuggerManager.getInstance(environment.project)
+            .startSession(
+                environment,
+                object : XDebugProcessStarter() {
+                    override fun start(session: XDebugSession): XDebugProcess {
+                        val debugProcess = MayaCharmDebugProcess(
+                                session,
+                                serverSocket,
+                                executionResult!!.executionConsole,
+                                executionResult.processHandler,
+                                false,
+                                sdk,
+                                process
+                        )
 
-        return null
+                        debugProcess.positionConverter = PyLocalPositionConverter()
+                        createConsoleCommunicationAndSetupActions(environment.project, executionResult, debugProcess, session)
+
+                        return debugProcess
+                    }
+                }
+            )
+
+        // TODO: good class to look at for a code example
+//        val attachRunner = PyAttachToProcessDebugRunner(environment.project, process.pid, result.homePath)
+//        attachRunner.launch()
+
+        return session.runContentDescriptor
     }
 }
