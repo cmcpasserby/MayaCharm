d99f713eb31b6aec5d9d17152d3d891b0894d2c9
Chris Cunningham <chris@rightsomegoodgames.ca>
2018-11-11 23:39:05 -0400

    added function for finding unused port numbers as well as some typealias for SdkPortMap and SdkPortPair

--- src/ca/rightsomegoodgames/mayacharm/settings/ApplicationSettings.kt
+++ src/ca/rightsomegoodgames/mayacharm/settings/ApplicationSettings.kt
@@ -1,14 +1,20 @@
 package ca.rightsomegoodgames.mayacharm.settings
 
 import com.intellij.openapi.components.*
+import com.jetbrains.python.sdk.PythonSdkType
+import java.util.*
+
+typealias SdkPortMap = MutableMap<String, Int>
+typealias SdkPortPair = Pair<String, Int>
+
+private val portRange = (4434..4534).toSet()
 
 @State(
         name = "MCAppSettings",
         storages = [Storage(value = "mayacharm.settings.xml", roamingType = RoamingType.DISABLED)]
 )
 class ApplicationSettings : PersistentStateComponent<ApplicationSettings.State> {
-    data class State(var mayaSdkMapping: Map<String, Int> = mapOf("Maya 2016" to 9000, "Maya 2017" to 9001))
-
+    data class State(var mayaSdkMapping: SdkPortMap = mutableMapOf())
     private var myState = State()
 
     companion object {
@@ -17,15 +23,49 @@ class ApplicationSettings : PersistentStateComponent<ApplicationSettings.State> 
         }
     }
 
+    init {
+        val mayaSdk = PythonSdkType.getAllLocalCPythons().filter { it.homePath?.endsWith("mayapy.exe") ?: false }
+        val homePaths = mayaSdk.map { it.homePath!! }
+
+        for (path in homePaths) {
+            myState.mayaSdkMapping[path] = -1
+        }
+
+        val freePorts = getFreePorts(myState.mayaSdkMapping)
+
+        for (key in myState.mayaSdkMapping.keys) {
+            myState.mayaSdkMapping[key] = freePorts.remove()
+        }
+    }
+
     override fun getState(): State {
         return myState
     }
 
     override fun loadState(state: State) {
-        myState.mayaSdkMapping = state.mayaSdkMapping
+        // todo figure out windows vs mac pathing here
+        val mayaPySdks = PythonSdkType.getAllLocalCPythons().filter { x -> x.homePath?.endsWith("mayapy.exe") ?: false }
+        val homePaths = mayaPySdks.map { x -> x.homePath!! }
+
+        myState.mayaSdkMapping.clear()
+
+        for (path in homePaths) {
+            if (state.mayaSdkMapping.containsKey(path)) {
+                myState.mayaSdkMapping[path] = state.mayaSdkMapping[path]!!
+                continue
+            }
+
+            myState.mayaSdkMapping[path] = -1
+        }
     }
 
-    var mayaSdkMapping: Map<String, Int>
+    var mayaSdkMapping: MutableMap<String, Int>
         get() = myState.mayaSdkMapping
         set(value) {myState.mayaSdkMapping = value}
+
+    private fun getFreePorts(data: SdkPortMap): Queue<Int> {
+        val usedPorts = data.map { it.value }.filter { it > 0 }.toSet()
+        val freePorts = portRange - usedPorts
+        return PriorityQueue<Int>(freePorts.sorted())
+    }
 }
--- src/ca/rightsomegoodgames/mayacharm/settings/MayaSdkConfigurable.kt
+++ src/ca/rightsomegoodgames/mayacharm/settings/MayaSdkConfigurable.kt
@@ -1,15 +1,10 @@
 package ca.rightsomegoodgames.mayacharm.settings
 
-import com.google.common.collect.ImmutableMap
-import com.google.common.collect.Maps
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.SearchableConfigurable
 import com.intellij.ui.*
 import com.intellij.util.ui.UIUtil
 import java.awt.BorderLayout
-import java.awt.Toolkit
-import javax.swing.JButton
 import javax.swing.JComponent
 
 class MayaSdkConfigurable : SearchableConfigurable, Configurable.NoScroll {
@@ -34,23 +29,23 @@ class MayaSdkConfigurable : SearchableConfigurable, Configurable.NoScroll {
 
     override fun isModified(): Boolean {
         val orgEntries = settings.mayaSdkMapping.toMap()
-        val newEntries = myPanel.data.map { it.key to it.value }.toMap()
+        val newEntries = myPanel.data.map{ it.first to it.second }.toMap()
         return newEntries != orgEntries
     }
 
     override fun reset() {
         myPanel.data.clear()
-        myPanel.data.addAll(settings.mayaSdkMapping.entries.toSet())
+        myPanel.data.addAll(settings.mayaSdkMapping.entries.map { it.key to it.value })
     }
 
     override fun apply() {
-        settings.mayaSdkMapping = myPanel.data.map { it.key to it.value }.toMap()
+        settings.mayaSdkMapping = myPanel.data.toMap().toMutableMap()
     }
 
     companion object {
         public const val ID = "ca.rightsomegoodgames.mayacharm.settings.MayaSdkConfigurable"
 
-        private class MayaPySdkTableModel : AddEditRemovePanel.TableModel<Map.Entry<String, Int>>() {
+        private class MayaPySdkTableModel : AddEditRemovePanel.TableModel<SdkPortPair>() {
             override fun getColumnCount(): Int {
                 return 2
             }
@@ -59,24 +54,24 @@ class MayaSdkConfigurable : SearchableConfigurable, Configurable.NoScroll {
                 return if (cIndex == 0) "Maya Version" else "Command Port"
             }
 
-            override fun getField(o: Map.Entry<String, Int>, cIndex: Int): Any {
-                return if (cIndex == 0) o.key else o.value
+            override fun getField(o: SdkPortPair, cIndex: Int): Any {
+                return if (cIndex == 0) o.first else o.second
             }
         }
 
         private val ourModel = MayaPySdkTableModel()
 
-        private class MayaPySdkTablePanel : AddEditRemovePanel<Map.Entry<String, Int>>(ourModel, arrayListOf()) {
-            override fun addItem(): Map.Entry<String, Int> {
-                return Maps.immutableEntry("Maya 2016", 4096)
+        private class MayaPySdkTablePanel : AddEditRemovePanel<SdkPortPair>(ourModel, arrayListOf()) {
+            override fun addItem(): Pair<String, Int> {
+                return "Maya 2016" to 4096
             }
 
-            override fun removeItem(o: Map.Entry<String, Int>): Boolean {
+            override fun removeItem(o: SdkPortPair): Boolean {
                 return false
             }
 
-            override fun editItem(o: Map.Entry<String, Int>): Map.Entry<String, Int> {
-                return Maps.immutableEntry("Maya 2016", o.value + 1)
+            override fun editItem(o: SdkPortPair): SdkPortPair {
+                return o.first to o.second + 1
             }
 
             override fun initPanel() {
@@ -95,5 +90,4 @@ class MayaSdkConfigurable : SearchableConfigurable, Configurable.NoScroll {
             }
         }
     }
-
 }
