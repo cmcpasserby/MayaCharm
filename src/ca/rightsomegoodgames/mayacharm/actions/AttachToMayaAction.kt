67d0b2ba205ec70824749d98d189bd1ca3ba948c
Chris Cunningham <chris@rightsomegoodgames.ca>
2018-12-09 23:58:38 -0400

    started work on the attach to maya option

--- src/ca/rightsomegoodgames/mayacharm/actions/AttachToMayaAction.kt
+++
@@ -1,9 +0,0 @@
-package ca.rightsomegoodgames.mayacharm.actions
-
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-
-class AttachToMayaAction : AnAction() {
-    override fun actionPerformed(e: AnActionEvent) {
-    }
-}
---
+++ src/ca/rightsomegoodgames/mayacharm/debug/MayaAttachDebuggerProvider.kt
@@ -0,0 +1,46 @@
+package ca.rightsomegoodgames.mayacharm.debug
+
+import ca.rightsomegoodgames.mayacharm.settings.ApplicationSettings
+import com.intellij.execution.process.ProcessInfo
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.util.UserDataHolder
+import com.intellij.xdebugger.attach.XAttachDebugger
+import com.intellij.xdebugger.attach.XAttachDebuggerProvider
+import com.intellij.xdebugger.attach.XAttachHost
+import com.jetbrains.python.debugger.attach.PyAttachToProcessDebugRunner
+import com.jetbrains.python.sdk.PythonSdkType
+
+
+class MayaAttachDebuggerProvider : XAttachDebuggerProvider {
+    override fun getAvailableDebuggers(project: Project, attachHost: XAttachHost, processInfo: ProcessInfo, userData: UserDataHolder): MutableList<XAttachDebugger> {
+        val sdks = ApplicationSettings.getInstance().mayaSdkMapping.values
+
+        val commandLine = processInfo.commandLine.removeSurrounding("\"")
+
+        if (!sdks.any { it.mayaPath == commandLine }) {
+            return mutableListOf()
+        }
+
+        val currentSdk = sdks.firstOrNull { it.mayaPath == commandLine } ?: return mutableListOf()
+        return mutableListOf(MayaAttachDebugger(PythonSdkType.findSdkByPath(currentSdk.mayaPyPath)!!))
+    }
+
+    override fun isAttachHostApplicable(attachHost: XAttachHost): Boolean {
+        return true
+    }
+}
+
+class MayaAttachDebugger(sdk: Sdk) : XAttachDebugger {
+    private val mySdkHome: String? = sdk.homePath
+    private val myName: String = "${PythonSdkType.getInstance().getVersionString(sdk)} ($mySdkHome)"
+
+    override fun getDebuggerDisplayName(): String {
+        return myName
+    }
+
+    override fun attachDebugSession(project: Project, attachHost: XAttachHost, processInfo: ProcessInfo) {
+        val runner = PyAttachToProcessDebugRunner(project, processInfo.pid, mySdkHome)
+        runner.launch()
+    }
+}
