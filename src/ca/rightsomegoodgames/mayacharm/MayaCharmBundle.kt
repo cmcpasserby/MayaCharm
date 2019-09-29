8a903c05dcf4ccb0dbb85ab86f87455abe0f31e5
Chris Cunningham <chris@rightsomegoodgames.ca>
2019-09-29 16:13:18 -0300

    added resource bundle for localization of strings

---
+++ src/ca/rightsomegoodgames/mayacharm/MayaCharmBundle.kt
@@ -0,0 +1,30 @@
+package ca.rightsomegoodgames.mayacharm
+
+import com.intellij.CommonBundle
+import com.intellij.reference.SoftReference
+import org.jetbrains.annotations.PropertyKey
+import java.lang.ref.Reference
+import java.util.*
+
+object MayaCharmBundle {
+    private const val BUNDLE = "ca.rightsomegoodgames.mayacharm.MayaCharmBundle"
+    private var ourBundle: Reference<ResourceBundle>? = null
+
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg parms: Any): String {
+        return CommonBundle.message(getBundle(), key, parms)
+    }
+
+    fun messageOfNull(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg parms: Any): String? {
+        return CommonBundle.messageOfNull(getBundle(), key, parms)
+    }
+
+    private fun getBundle(): ResourceBundle {
+        var bundle = SoftReference.dereference(ourBundle)
+
+        if (bundle == null) {
+            bundle = ResourceBundle.getBundle(BUNDLE)
+            ourBundle = SoftReference(bundle)
+        }
+        return bundle!!
+    }
+}
