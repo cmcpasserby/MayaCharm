59c1d58c8ebeeab63a67a8c630ae58bafd45852f
Chris Cunningham <chris@rightsomegoodgames.ca>
2021-02-15 14:34:52 -0400

    loc fixes

--- src/main/kotlin/MayaBundle.kt
+++ src/main/kotlin/MayaBundle.kt
@@ -1,28 +1,13 @@
-import com.intellij.CommonBundle
-import com.intellij.reference.SoftReference
+import com.intellij.DynamicBundle
 import org.jetbrains.annotations.PropertyKey
-import java.lang.ref.Reference
-import java.util.*
 
-object MayaBundle {
-    private const val BUNDLE = "MayaCharmBundle"
-    private var ourBundle: Reference<ResourceBundle>? = null
+class MayaBundle : DynamicBundle(BUNDLE) {
+    companion object {
+        private const val BUNDLE = "MayaCharmBundle"
+        private val INSTANCE = MayaBundle()
 
-    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg parms: Any): String {
-        return CommonBundle.message(getBundle(), key, *parms)
-    }
-
-    fun messageOrNull(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg parms: Any): String? {
-        return CommonBundle.messageOrNull(getBundle(), key, *parms)
-    }
-
-    private fun getBundle(): ResourceBundle {
-        var bundle = SoftReference.dereference(ourBundle)
-
-        if (bundle == null) {
-            bundle = ResourceBundle.getBundle(BUNDLE)
-            ourBundle = SoftReference(bundle)
+        fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg parms: Any): String {
+            return INSTANCE.getMessage(key, *parms)
         }
-        return bundle!!
     }
 }
